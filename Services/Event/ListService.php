424243dda3e2502806dc44df16075b730c5a9d36
Dumitru Boaghi <boaghi.dumitru@gmail.com>
2017-07-01 11:02:33 +0300

    update6.2.6

--- Services/Event/ListService.php
+++ Services/Event/ListService.php
@@ -69,9 +69,7 @@ class ListService implements IQuarkGetService, IQuarkPostService, IQuarkServiceW
 					'id',
 					'name',
 					'startdate'
-				)
-			)
-//		, 'number' =>$event->Count()
+				))
 		);
 	}
 }
