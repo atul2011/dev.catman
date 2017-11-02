3c6788766762269cd9a491185b4eb790d009f7f4
dboaghi <boaghi.dumitru@gmail.com>
2017-11-02 15:19:28 +0200

    update7.9.11

--- Views/Admin/Content/Article/Edit.php
+++ Views/Admin/Content/Article/Edit.php
@@ -38,6 +38,7 @@ use ViewModels\Admin\Content\Article\CreateView;
                                 echo '<option value="' , Article::TYPE_DECREE , '" ' , $article->type == Article::TYPE_DECREE ? 'selected' : '' , '>Decree</option>';
                                 echo '<option value="' , Article::TYPE_EXCERPT , '" ' , $article->type == Article::TYPE_EXCERPT ? 'selected' : '' , '>Excerpt</option>';
                                 echo '<option value="' , Article::TYPE_QUESTION , '" ' , $article->type == Article::TYPE_QUESTION ? 'selected' : '' , '>Question</option>';
+                                echo '<option value="' , Article::TYPE_MESSAGE , '" ' , $article->type == Article::TYPE_MESSAGE ? 'selected' : '' , '>Message</option>';
                             ?>
                         </select>
 					</div>
