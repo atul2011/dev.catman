ff8a9b1304a988deef5db8b85963b86e5df475fb
Boaghi Dumitru <boaghi.dumitru@gmail.com>
2019-03-29 12:04:24 +0200

    Add Priority set in link list

--- Views/Admin/Link/List.php
+++ Views/Admin/Link/List.php
@@ -41,10 +41,11 @@ elseif ($target_type == Link::TARGET_TYPE_ARTICLE) {
     <div class="quark-presence-container presence-block main2 items-list" id="event-list">
         <div class="quark-presence-column" id="content-column">
             <div class="quark-presence-container presence-block" id="list-content">
-                <div id="ID" class="quark-presence-column content-titles ids">ID</div>
-                <div id="title" class="quark-presence-column  content-titles titles">Title</div>
-                <div id="link" class="quark-presence-column  content-titles links">Link</div>
-                <div id="actions" class="quark-presence-column  content-titles actions">Actions</div>
+                <div id="ID" class="quark-presence-column content-titles ids">ID
+                </div><div id="title" class="quark-presence-column content-titles titles">Title
+                </div><div id="link" class="quark-presence-column content-titles links">Link
+                </div><div id="link" class="quark-presence-column content-titles priorities">Priority
+                </div><div id="actions" class="quark-presence-column content-titles actions">Actions</div>
             </div>
             <?php
             foreach ($links as $link) {
@@ -53,6 +54,7 @@ elseif ($target_type == Link::TARGET_TYPE_ARTICLE) {
                     '<div class="content-values quark-presence-column ids">' , $link->id , '</div>' ,
                     '<div class="content-values quark-presence-column titles">' , $link->title  , '</div>' ,
                     '<div class="content-values quark-presence-column links">' , $link->link , '</div>' ,
+                    '<div class="content-values quark-presence-column priorities" item-id="' , $link->id , '">' , $link->priority , '</div>' ,
                     '<div class="content-values quark-presence-column actions">' ,
                         '<a class="fa actions edit-button-link fa-pencil content-actions " id="edit-link-' , $link->id , '" href="/admin/link/edit/' , $link->id , '"></a>' ,
                         '<a class="fa actions delete-button-link fa-trash content-actions item-remove-dialog" quark-dialog="#item-remove" quark-redirect="/admin/link/list/" id="delete-link-' , $link->id , '" href="/admin/link/delete/' , $link->id , '"></a>' ,
--- static/Admin/Link/List/index.js
+++ static/Admin/Link/List/index.js
@@ -6,3 +6,18 @@ $(document).ready(function(){
     });
 
 });
+
+$(document).on('click', '.content-values.priorities', function () {
+    $(this).attr('contenteditable', 'true');
+});
+
+$(document).on('keypress', '.content-values.priorities', function (e) {
+    var item = $(this);
+    if (isNaN(String.fromCharCode(e.which))) e.preventDefault();
+
+    if (e.keyCode == 13) {
+        $.ajax({url:'/admin/link/edit/' + item.attr('item-id'), type:'POST', data:{'priority' : item.html()}}).then(function (data) {
+            window.location.replace(window.location.href);
+        });
+    }
+});
